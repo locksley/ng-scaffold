62900c3b22feeee8e203b0c17dcc1964ba783fb5
Scott Li <scottli0101@gmail.com>
2014-09-15 23:03:36 -0400

    clean gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,20 +23,14 @@ module.exports = function(grunt) {
   grunt.initConfig(configTasks);
 
   // CLI Tasks
-  grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'wiredep',
-      'concurrent:server',
-      'autoprefixer',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
+  grunt.registerTask('serve', [
+    'clean:server',
+    'wiredep',
+    'concurrent:server',
+    'autoprefixer',
+    'connect:livereload',
+    'watch'
+  ]);
 
   grunt.registerTask('test', [
     'clean:server',
